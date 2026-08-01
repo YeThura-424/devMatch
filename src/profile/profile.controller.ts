5ace109d83ac3af5984ee120c4ed5cc171a49bc8
YeThura-424 <68313967+YeThura-424@users.noreply.github.com>
2026-08-01 21:51:44 +0400

    add: profile detail and create api method

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { ProfileCreateDTO } from './dto/create-profile.dto';
 
 @Controller('profile')
 export class ProfileController {
@@ -7,5 +8,19 @@ export class ProfileController {
   @Get()
   findAll(@Query('age') age: number, @Query('location') location: string) {
     return [{ age, location }];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() createProfileDTO: ProfileCreateDTO) {
+    return {
+      name: createProfileDTO.name,
+      age: createProfileDTO.age,
+      location: createProfileDTO.location,
+    };
   }
 }
