7f3623e51f7048b4000882c5a9359cd28ca22cdf
YeThura-424 <68313967+YeThura-424@users.noreply.github.com>
2026-08-15 21:43:58 +0400

    add: validation in profile create dto

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,5 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsNumber, IsString, Length, Max, Min } from 'class-validator';
+
 export class ProfileCreateDTO {
+  @IsString({ message: 'Name must be a string.' })
+  @Length(3, 20, { message: 'Name must be between 3 and 20 characters long.' })
   name!: string;
+
+  @IsNumber({}, { message: 'Age must be a number.' })
+  @Min(0, { message: 'Age must be a non-negative number.' })
+  @Max(120, { message: 'Age must be less than or equal to 120.' })
   age!: number;
+
+  @IsString({ message: 'Location must be a string.' })
   location!: string;
 }
